07a48c7b094dd67fa7f8ac251bb4db2eae887c35
Connor <connorjang20@gmail.com>
2017-01-20 15:59:22 -0800

    Testing commits

--- app/src/main/java/a00953080/comp3717/bcit/ca/servicefinder/MainActivity.java
+++ app/src/main/java/a00953080/comp3717/bcit/ca/servicefinder/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    // testing 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
